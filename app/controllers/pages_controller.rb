d5586f31c5a32657c109f44845bd148325c04445
andylinteau <andy.linteau@gmail.com>
2015-02-06 04:51:54 -0500

    add pages and made breadcrumb dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,33 @@
 class PagesController < ApplicationController
 
 	def home
+				@bread_crumb = false
+
 	end
 
 	def contact
+		@bread_crumb = true
+		@page_name = "Contact"
 	end
 
 	def about
+		@bread_crumb = true
+		@page_name = "About"
+	end
+
+	def getfood
+		@bread_crumb = true
+		@page_name = "Get Food"
+	end
+
+	def donate
+		@bread_crumb = true
+		@page_name = "Donate"
+	end
+
+	def volunteer
+		@bread_crumb = true
+		@page_name = "Volunteer"
 	end
 	
 end
