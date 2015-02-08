55cf6ee2332505738c3e29a20805fef299de04e3
andylinteau <andy.linteau@gmail.com>
2015-02-07 22:26:50 -0500

    add simple form and get mailer working locally

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,20 @@
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
+    @page_name = "Contact"
+    @bread_crumb = true
   end
 
   def create
+    @page_name = "Contact"
+    @bread_crumb = true
     @contact = Contact.new(params[:contact])
     @contact.request = request
+    redirect_to contacts_path
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash[:success] = 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = 'Cannot send message.'
+      flash[:error] = 'Cannot send message.'
       render :new
     end
   end
