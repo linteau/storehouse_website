32194c238b8e6748769d3b8ee096d6866303a1b3
andylinteau <andy.linteau@gmail.com>
2015-02-28 01:16:11 -0500

    final edit to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
 	def home
 				@bread_crumb = false
 				@active_home = 'active'
-
 	end
 
 	def contact
@@ -22,20 +21,21 @@ class PagesController < ApplicationController
 		@bread_crumb = true
 		@page_name = "Get Food"
 		@active_getfood = "active"
-
+		@bible = 'bible' 
 	end
 
 	def donate
 		@bread_crumb = true
 		@page_name = "Donate"
 		@active_donate = "active"
+		@bible = 'bible' 
 	end
 
 	def volunteer
 		@bread_crumb = true
 		@page_name = "Volunteer"
 		@active_volunteer = "active"
-
+		@bible = 'bible' 
 	end
 	
 end
